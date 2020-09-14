96f459324957837c48a72d3aec82c5fcbf641193
Mees Buschman <mezeman1@gmail.com>
2020-09-14 16:45:45 +0200

    Level 1 example completed

---
+++ Level 1/MadLevel1Example/app/src/main/java/com/example/madlevel1example/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.madlevel1example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        binding.btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
+    }
+
+    /**
+     * Check if the submitted answer is correct.
+     */
+    private fun checkAnswer() {
+        val answer = binding.etAnswer.text.toString()
+        println(answer)
+
+        // When the answer equals "giraffe" then display a correct message using a toast message.
+        // Otherwise display an incorrect message.
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+}
