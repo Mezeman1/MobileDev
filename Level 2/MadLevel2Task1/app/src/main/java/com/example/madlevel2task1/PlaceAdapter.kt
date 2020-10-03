8bfb1e2c071088edde9c200729b879f04a5d8fc0
Mees Buschman <mezeman1@gmail.com>
2020-10-03 15:30:04 +0200

    Finalize task 1

--- Level 2/MadLevel2Task1/app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
+++ Level 2/MadLevel2Task1/app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
@@ -1,25 +1,35 @@
 package com.example.madlevel2task1
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.databinding.ItemPlaceBinding
 
 class PlaceAdapter(private val places: List<Place>) :
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
 
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemPlaceBinding.bind(itemView)
+
+        fun bind(place: Place) {
+            binding.tvPlace.text = place.name
+            binding.ivPlace.setImageResource(place.imageResId)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(places[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return places.size
     }
 
 }
