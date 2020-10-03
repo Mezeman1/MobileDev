7387264cb00a22c01f193e8b74eb81e233e2ce2a
Mees Buschman <mezeman1@gmail.com>
2020-10-03 15:12:35 +0200

    PlaceAdapter init

---
+++ Level 2/MadLevel2Task1/app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.madlevel2task1
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PlaceAdapter(private val places: List<Place>) :
+    RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
